00c92b84f8b166dfb203e5a9cb1525b8403490a3
MikelPedro <mikel.pedrolinaje@gmail.com>
2023-11-19 19:06:39 +0100

    Funcionamiento correcto nginx + pantalla de cities

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,16 @@ FROM python:3-alpine3.15
 
 WORKDIR /entrega
 
-COPY . /entrega/
+# Copia los archivos de la aplicación Flask
+COPY . /entrega//
 
+# Instala las dependencias
 RUN pip install -r requirements.txt
 
 COPY . .
 
-CMD ["gunicorn", "webtiempo:app", "-b", "0.0.0.0:5000"]
+# Expone el puerto en el que Flask se ejecutará
+EXPOSE 5000
+
+# Comando para ejecutar la aplicación Flask
+CMD ["python", "webtiempo.py"]
--- webtiempo.py
+++ webtiempo.py
@@ -1,19 +1,24 @@
 from flask import Flask, render_template, request, flash
 from tiempo import main as get_datosTiempo
+import pymongo
 from pymongo import MongoClient
 
 app = Flask(__name__, template_folder='./app')
-app.config['SECRET_KEY'] = 'SecretoLlave'
-app.config['MONGO_URI'] = 'mongodb://db:27017/'
+app.config['SECRET_KEY'] = 'SecretoLlave' # esto es para le flash
+#app.config['MONGO_URI'] = 'mongodb://db:27017/'
 #conectarse a la bd
-mongo = MongoClient(app.config['MONGO_URI'])
+
+#client = MongoClient(host='test_mongodb', port=27017, user='root', password='pass', authSource='admin')
+#db = client['datostemp']
+#return db
+
 
 URL_ORIGINAL = "https://api.openweathermap.org/data/2.5/weather?id="
 API_KEY = open('api_key','r').read()
 
-def ciudad_existe(ciudad):
+#def ciudad_existe(ciudad):
     # Verifica si la ciudad ya existe en la base de datos
-    return mongo.db.ciudades.find_one({'lugar': ciudad}) is not None
+#    return mongo.db.ciudades.find_one({'lugar': ciudad}) is not None
 
 @app.route('/', methods=[ 'GET', 'POST'])
 def index():
@@ -21,7 +26,6 @@ def index():
     error_msg = ''
     datosTiempo = None
     ciudad = None
-
     if request.method == 'POST':
         ciudad = request.form['ciudad']
         print(f"Ciudad recibida: {ciudad}")
@@ -29,14 +33,14 @@ def index():
 
         if not ciudad:
             error_msg = "Introduzca una ciudad"
-        elif ciudad_existe(ciudad):
-            error_msg = f'La ciudad {ciudad} ya está en la base de datos'
+            #elif ciudad_existe(ciudad):
+            #    error_msg = f'La ciudad {ciudad} ya está en la base de datos'
         else:
             datosTiempo = get_datosTiempo(ciudad)
             if datosTiempo == 'Error':
                 error_msg = 'Esa no es una ciudad válida!'
             
-            mongo.db.ciudades.insert_one(datosTiempo.__dict__)
+            #db.ciudades.insert_one(datosTiempo.__dict__)
 
         if error_msg:
             flash(error_msg, 'error')
@@ -44,10 +48,15 @@ def index():
             flash('Ciudad añadida exitosamente!', 'success')
 
        
-    ciudades_guardadas = mongo.db.ciudades.find()
-    return render_template('index.html', data=datosTiempo, datos_save=ciudades_guardadas)
+   # ciudades_guardadas = mongo.db.ciudades.find()
+    return render_template('index.html', data=datosTiempo)
+
+@app.route('/cities')
+def cities():
+    #cities_data = list(db.ciudades.find())
+    return render_template('cities.html')
 
 
 if __name__ == '__main__':
 
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
